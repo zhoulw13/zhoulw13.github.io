3460a251b89756b250918bfedb7dc246be84fc5d
zhoulw13 <zhoulw063922@gmail.com>
2015-07-14 00:19:03 +0800

    add json file

---
+++ hw/waterfall.Ajax/json/img_name1.js
@@ -0,0 +1,24 @@
+{
+	"images":[
+	{"id":1, "url":"../image/showLove (1).jpg", "column":1},
+	{"id":2, "url":"../image/showLove (2).jpg", "column":2},
+	{"id":3, "url":"../image/showLove (3).jpg", "column":3},
+	{"id":4, "url":"../image/showLove (4).jpg", "column":1},
+	{"id":5, "url":"../image/showLove (5).jpg", "column":2},
+	{"id":6, "url":"../image/showLove (6).jpg", "column":3},
+	{"id":7, "url":"../image/showLove (7).jpg", "column":1},
+	{"id":8, "url":"../image/showLove (8).jpg", "column":2},
+	{"id":9, "url":"../image/showLove (9).jpg", "column":3},
+	{"id":10, "url":"../image/showLove (10).jpg", "column":1},
+	{"id":11, "url":"../image/showLove (11).jpg", "column":2},
+	{"id":12, "url":"../image/showLove (12).jpg", "column":3},
+	{"id":13, "url":"../image/showLove (13).jpg", "column":1},
+	{"id":14, "url":"../image/showLove (14).jpg", "column":2},
+	{"id":15, "url":"../image/showLove (15).jpg", "column":3},
+	{"id":16, "url":"../image/showLove (16).jpg", "column":1},
+	{"id":17, "url":"../image/showLove (17).jpg", "column":2},
+	{"id":18, "url":"../image/showLove (18).jpg", "column":3},
+	{"id":19, "url":"../image/showLove (19).jpg", "column":1},
+	{"id":20, "url":"../image/showLove (20).jpg", "column":2}
+	]
+}
---
+++ hw/waterfall.Ajax/json/img_name2.js
@@ -0,0 +1,19 @@
+{
+	"images":[
+	{"id":1, "url":"../image/showLove (1).jpg"},
+	{"id":2, "url":"../image/showLove (2).jpg"},
+	{"id":3, "url":"../image/showLove (3).jpg"},
+	{"id":4, "url":"../image/showLove (4).jpg"},
+	{"id":5, "url":"../image/showLove (5).jpg"},
+	{"id":6, "url":"../image/showLove (6).jpg"},
+	{"id":7, "url":"../image/showLove (7).jpg"},
+	{"id":8, "url":"../image/showLove (8).jpg"},
+	{"id":9, "url":"../image/showLove (9).jpg"},
+	{"id":10, "url":"../image/showLove (10).jpg"},
+	{"id":11, "url":"../image/showLove (11).jpg"},
+	{"id":12, "url":"../image/showLove (12).jpg"},
+	{"id":13, "url":"../image/showLove (13).jpg"},
+	{"id":14, "url":"../image/showLove (14).jpg"},
+	{"id":15, "url":"../image/showLove (15).jpg"},
+	]
+}
