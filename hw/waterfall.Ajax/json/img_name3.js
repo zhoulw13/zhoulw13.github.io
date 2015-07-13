82eefb2e1999f132454f4c1375c29f19b7e2885e
zhoulw13 <zhoulw063922@gmail.com>
2015-07-14 01:30:08 +0800

    get json

---
+++ hw/waterfall.Ajax/json/img_name3.js
@@ -0,0 +1,24 @@
+{
+	"images":[
+	{"id":41, "url":"image/showLove (41).jpg", "column":1},
+	{"id":42, "url":"image/showLove (42).jpg", "column":2},
+	{"id":43, "url":"image/showLove (43).jpg", "column":3},
+	{"id":44, "url":"image/showLove (44).jpg", "column":1},
+	{"id":45, "url":"image/showLove (45).jpg", "column":2},
+	{"id":46, "url":"image/showLove (46).jpg", "column":3},
+	{"id":47, "url":"image/showLove (47).jpg", "column":1},
+	{"id":48, "url":"image/showLove (48).jpg", "column":2},
+	{"id":49, "url":"image/showLove (49).jpg", "column":3},
+	{"id":50, "url":"image/showLove (50).jpg", "column":1},
+	{"id":51, "url":"image/showLove (51).jpg", "column":2},
+	{"id":52, "url":"image/showLove (52).jpg", "column":3},
+	{"id":53, "url":"image/showLove (53).jpg", "column":1},
+	{"id":54, "url":"image/showLove (54).jpg", "column":2},
+	{"id":55, "url":"image/showLove (55).jpg", "column":3},
+	{"id":56, "url":"image/showLove (56).jpg", "column":1},
+	{"id":57, "url":"image/showLove (57).jpg", "column":2},
+	{"id":58, "url":"image/showLove (58).jpg", "column":3},
+	{"id":59, "url":"image/showLove (59).jpg", "column":2},
+	{"id":60, "url":"image/showLove (60).jpg", "column":3}
+	]
+}
