380e11fcfe6dea2c039f8f920027dcd1d85a6c5c
zhoulw13 <zhoulw063922@gmail.com>
2015-07-14 11:24:13 +0800

    get json

--- hw/waterfall.Ajax/script/Ajax.js
+++ hw/waterfall.Ajax/script/Ajax.js
@@ -1,5 +1,5 @@
 function getImg(i){
-	var str = "json/img_name"+i+".js";
+	var str = "https://raw.githubusercontent.com/zhoulw13/zhoulw13.github.io/master/hw/waterfall.Ajax/json/img_name"+i+".js";
 	$.getJSON(str, callback);
 }
 
@@ -9,11 +9,28 @@ function callback(data){
 			return;
  		var li = $('#ul1 li:first');
  		var c = li.clone(true);
- 		c.children().attr("href", item.url);
+ 		//c.children().attr("href", item.url);
  		c.children().children().attr("src", item.url);
  		c.children().children().attr("id", "img"+item.id);
  		var str = "#ul" + item.column;
  		var ul = $(str);
  		ul.append(c);
   });
+}
+
+var i = 2;
+
+$(window).scroll(function(){
+	 if($(document).height() - $(window).height() - $(document).scrollTop() < 10) {
+		if(i==5)
+			return;
+		getImg(i);
+		i++;
+	}
+});
+
+
+function imageclicked(evt){
+	console.log(evt);
+	$('body').attr('class', 'popBg');
 }
--- hw/waterfall.Ajax/script/layImg.js
+++ hw/waterfall.Ajax/script/layImg.js
@@ -1,25 +1,17 @@
-var ul1 = $('#ul1');
-var ul2 = $('#ul2');
-var ul3 = $('#ul3');
-var li = $('#ul1 li:first');
-for(var i = 1; i<=29; i++){
-	var c1 = li.clone(true);
-	var c2 = li.clone(true);
-	var c3 = li.clone(true);
-	c1.children().attr("href", "image/showLove ("+(3*i+1)+").jpg");
-	c2.children().attr("href", "image/showLove ("+(3*i+2)+").jpg");
-	c3.children().attr("href", "image/showLove ("+(3*i+3)+").jpg");
-	c1.children().children().attr("src", "image/showLove ("+(3*i+1)+").jpg");
-	c2.children().children().attr("src", "image/showLove ("+(3*i+2)+").jpg");
-	c3.children().children().attr("src", "image/showLove ("+(3*i+3)+").jpg");
-	ul1.append(c1);
-	ul2.append(c2);
-	ul3.append(c3);
+function loadImg(s, e){
+	var ul1 = $('#ul1');
+	var ul2 = $('#ul2');
+	var ul3 = $('#ul3');
+	var li = $('#ul1 li:first');
+		for(var i = s; i<=e; i++){
+		var c1 = li.clone(true);
+		var c2 = li.clone(true);
+		var c3 = li.clone(true);
+		c1.children().children().attr("src", "images/showLove ("+(3*i+1)+").jpg");
+		c2.children().children().attr("src", "images/showLove ("+(3*i+2)+").jpg");
+		c3.children().children().attr("src", "images/showLove ("+(3*i+3)+").jpg");
+		ul1.append(c1);
+		ul2.append(c2);
+		ul3.append(c3);
+	}
 }
-
-var li = $('#ul1 li:last');
-$('#ul2').append(li);
-var li = $('#ul1 li:last');
-$('#ul2').append(li);
-var li = $('#ul1 li:last');
-$('#ul2').append(li);
