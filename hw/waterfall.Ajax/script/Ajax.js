d8290a684db17b0e987458bd36c688ddd6b8602d
zhoulw13 <zhoulw063922@gmail.com>
2015-07-14 15:25:22 +0800

    comments add

--- hw/waterfall.Ajax/script/Ajax.js
+++ hw/waterfall.Ajax/script/Ajax.js
@@ -22,15 +22,42 @@ var i = 2;
 
 $(window).scroll(function(){
 	 if($(document).height() - $(window).height() - $(document).scrollTop() < 10) {
-		if(i==5)
+		if(i===5)
 			return;
 		getImg(i);
 		i++;
 	}
 });
 
+var mousemove = window.onmousewheel;
+var dmousemove = document.onmousewheel;
 
 function imageclicked(evt){
-	console.log(evt);
-	$('body').attr('class', 'popBg');
+	var e = window.event;
+    var obj = e.target || e.srcElement;
+	$('body').prepend("<div class='popBg'></div>");
+	$('body').append("<img class='popWrap' src='"+ obj.src +"'>");
+	$('body').append("<div class='comments'></div>");
+	var img = $('.popWrap');
+	if(img.height() > $(window).height()*0.8)
+		$(img).css("height",$(window).height()*0.8+"px");
+	$(img).css("top", parseInt(($(window).height() - img.height())/2) + "px");
+	$(img).css("left", parseInt(($(window).width() - img.width())/3) + "px");
+
+	var comment = $('.comments');
+	$(comment).css("width", parseInt(($(window).width() - img.width())/3)+"px");
+	$(comment).css("left", parseInt($(window).width()/3 + img.width()*2/3) + "px");
+
+	window.onmousewheel = document.onmousewheel = function () {return false;}
+	$('.popBg').click(function(){
+		$('.popBg').remove();
+		$('.popWrap').remove();
+		$('.comments').remove();
+		window.onmousewheel = mousemove;
+		document.onmousewheel = dmousemove;
+	});
+}
+
+function getComments(i){
+	
 }
