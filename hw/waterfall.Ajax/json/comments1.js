86e3d6f32d68d870192b31ea35b8f29a019cee39
zhoulw13 <zhoulw063922@gmail.com>
2015-09-25 00:05:08 +0800

    hw into independent gh-pages

--- hw/waterfall.Ajax/json/comments1.js
+++
@@ -1,9 +0,0 @@
-﻿{
-	"comments":[
-	{"id":1, "author":"rp", "content":"沙发", "date":"2015-7-14", "device":"iOS"},
-	{"id":2, "author":"峰兄", "content":"分得快！", "date":"2015-7-12", "device":"Android"},
-	{"id":3, "author":"桐姐", "content":"我们是糖，甜到哀伤", "date":"2015-6-30", "device":"Android"},
-	{"id":4, "author":"丧病", "content":"是不是说一句我想你就可以永远不分离。", "date":"2015-6-29", "device":"Windows Phone"},
-	{"id":5, "author":"坚哥", "content":"烧烧烧！", "date":"2015-6-13", "device":"网页客户端"}
-	]
-}
--- hw/waterfall.Ajax/json/comments2.js
+++
@@ -1,9 +0,0 @@
-﻿{
-	"comments":[
-	{"id":6, "author":"xp", "content":"这是我见过的点赞最多的秀恩爱", "date":"2015-5-27", "device":"Android"},
-	{"id":7, "author":"我孙子", "content":"如何评价室友在寝室打电话等秀恩爱的行为？ - 人际交往- 知乎", "date":"2015-5-15", "device":"iOS"},
-	{"id":8, "author":"大白菜", "content":"你们开心就好！", "date":"2015-4-20", "device":"网页客户端"},
-	{"id":9, "author":"隔壁老王", "content":"后面的朋友，你们的火把呢！", "date":"2015-4-1", "device":"iOS"},
-	{"id":10, "author":"锁哥", "content":"秀的飞起", "date":"2015-1-28", "device":"Android"}
-	]
-}
